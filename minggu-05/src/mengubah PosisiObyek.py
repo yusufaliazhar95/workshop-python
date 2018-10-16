6b7dab2db6e6f25a632850e491d0767c5fcf02c7
yusufaliazhar95 <yusufaliazha95@gmail.com>
2018-10-16 15:22:37 +0700

    src

---
+++ minggu-05/src/mengubah PosisiObyek.py
@@ -0,0 +1,10 @@
+f = open('workfile', 'rb+')
+f.write(b'0123456789abcdef')
+
+f.seek(5)      # Go to the 6th byte in the file
+
+f.read(1)
+
+f.seek(-3, 2)  # Go to the 3rd byte before the end
+
+f.read(1)
